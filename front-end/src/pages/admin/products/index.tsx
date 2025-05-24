1283ab59eb4c3314b85d7db749d5500e88528548
LAPTOP-G4JJ8UTM\Phan_Hoang_Khang <KhangPhan1121>
2025-05-24 15:39:52 +0700

    Fix button Import Excel

--- front-end/src/pages/admin/products/index.tsx
+++ front-end/src/pages/admin/products/index.tsx
@@ -1216,7 +1216,7 @@ function AdminProducts({
               className="flex items-center px-4 py-2 bg-blue-600 text-white rounded-md hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500"
             >
               <FiUpload className="mr-2" />
-              Import Excel
+              Nhập Excel
             </button>
           </div>
           <button
